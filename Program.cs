4cc668fa8de2fa3c2de3057fdfe817975734c9b1
AQ232596 <AQ232596@S81_11>
2023-09-12 13:42:59 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double Width = 0;
+            double Height = 0;
+
+            const double max_width = 5.0;
+
+            Console.Write("Enter Width of the Window: ");
+            Width = double.Parse(Console.ReadLine());
+            if (Width > max_width)
+            {
+                Console.WriteLine("Too Wide");
+                Width = max_width;
+            }
+        }
+    }
+}
